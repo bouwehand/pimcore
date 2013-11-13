343d4c1bbb056bfa4e817b1c7a0a3a9780667032
B.ouwehand <b.ouwehand@synocom.nl>
2013-11-13 16:52:04 +0100

    FULL INSTALL WITH ALL SAMPLE DATA

---
+++ pimcore/models/Translation/Admin.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Pimcore
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.pimcore.org/license
+ *
+ * @category   Pimcore
+ * @package    Translation
+ * @copyright  Copyright (c) 2009-2013 pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     New BSD License
+ */
+
+class Translation_Admin extends Translation_Abstract {
+
+    /**
+     * Static Helper to get the translation of the current logged in user
+     *
+     * @static
+     * @param $id - translation key
+     * @param bool $create - creates an empty translation entry if the key doesn't exists
+     * @param bool $returnIdIfEmpty - returns $id if no translation is available
+     * @return string
+     * @throws Exception
+     */
+    public static function getByKeyLocalized($id, $create = false, $returnIdIfEmpty = false)
+    {
+        if(Zend_Registry::isRegistered("Zend_Locale")) {
+            $language = (string) Zend_Registry::get("Zend_Locale");
+        } else if($user = Pimcore_Tool_Admin::getCurrentUser()) {
+            $language = $user->getLanguage();
+        } else if ($user = Pimcore_Tool_Authentication::authenticateSession()) {
+            $language = $user->getLanguage();
+        } else {
+            $config = Pimcore_Config::getSystemConfig();
+            $language = $config->language;
+        }
+
+        return self::getByKey($id, $create, $returnIdIfEmpty)->getTranslation($language);
+    }
+}
---
+++ website/views/scripts/content/portal.php
@@ -0,0 +1,61 @@
+
+<?php $this->headLink(array(
+    "rel" => "stylesheet",
+    "href" => "/website/static/css/portal.css"));
+?>
+
+<div id="myCarousel" class="carousel slide">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <?php
+        $count = $this->select("carouselSlides")->getData();
+        if(!$count) {
+            $count = 1;
+        }
+        for($i=0; $i<$count; $i++) { ?>
+            <div class="item<?php if(!$i) { ?> active<?php } ?>">
+                <?php echo $this->image("cImage_".$i, array())->frontend(); ?>
+                <div class="container">
+                    <div class="carousel-caption">
+                        <?php
+                        if($this->editmode) {
+                            echo $this->image("cImage_".$i, array(
+                                "reload" => true,
+                                "hidetext" => true,
+                                "title" => "Drag Image Here",
+                                "height" => 30
+                            ));
+                            echo "<br /><br />";
+                        }
+                        ?>
+
+                        <h1><?php echo $this->input("cHeadline_".$i, array("width" => 900)); ?></h1>
+                        <div class="caption"><?php echo $this->textarea("cText_".$i, array("width" => 900)); ?></div>
+                        <div class="margin-bottom-10"><?php echo $this->link("cLink_".$i, array("class" => "btn btn-large btn-default")); ?></div>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+
+<?php if($this->editmode) { ?>
+    <div class="container" style="padding-bottom: 40px">
+        Number of Slides: <?php echo $this->select("carouselSlides", array(
+            "width" => 60,
+            "reload" => true,
+            "store" => array(array(1,1),array(2,2),array(3,3), array(4,4))
+        )); ?>
+    </div>
+<?php } ?>
+
+<div class="container">
+    <?php echo $this->areablock("content"); ?>
+</div>
